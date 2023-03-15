61ff4375fc0cf7e0ebafd55d56069e89bc94d4d4
kagakubu22A <kagakubu.22a@gmail.com>
2023-03-15 18:31:30 +0900

    extra function deleted

--- lib/thirdParty/TileInfo.cpp
+++ lib/thirdParty/TileInfo.cpp
@@ -41,25 +41,3 @@ Wall TileInfo::GetWall(Direction dir)
 		throw std::invalid_argument("TileInfo::GetWall");
 	}
 }
-
-void TileManager::Initialize(Walls walls)
-{
-	_tilevec.push_back(TileInfo{Point{0, 0}, walls, vector<Obstacle>{Obstacle::Nothing}, FloorType::StartPoint});
-	M5.Lcd.println();
-	/*_tilevec.push_back(TileInfo(Point(0, 0), Obstacle::Nothing, vector<Wall>{Wall::WallNotExists, Wall::WallNotExists, Wall::WallExists, Wall::WallExists}));
-	_tilevec.push_back(TileInfo(Point(-1, 0), Obstacle::Nothing, vector<Wall>{Wall::WallNotExists, Wall::WallNotExists, Wall::WallNotExists, Wall::WallExists}));
-	_tilevec.push_back(TileInfo(Point(-2, 0), Obstacle::Nothing, vector<Wall>{Wall::WallExists, Wall::WallNotExists, Wall::WallNotExists, Wall::WallExists}));
-
-	_tilevec.push_back(TileInfo(Point(0, 1), Obstacle::Nothing, vector<Wall>{Wall::WallNotExists, Wall::WallNotExists, Wall::WallExists, Wall::WallNotExists}));
-	_tilevec.push_back(TileInfo(Point(-1, 1), Obstacle::Nothing, vector<Wall>{Wall::WallNotExists, Wall::WallNotExists, Wall::WallNotExists, Wall::WallNotExists}));
-	_tilevec.push_back(TileInfo(Point(-2, 1), Obstacle::Nothing, vector<Wall>{Wall::WallExists, Wall::WallNotExists, Wall::WallNotExists, Wall::WallNotExists}));
-
-	_tilevec.push_back(TileInfo(Point(0, 2), Obstacle::Nothing, vector<Wall>{Wall::WallNotExists, Wall::WallExists, Wall::WallExists, Wall::WallNotExists}));
-	_tilevec.push_back(TileInfo(Point(-1, 2), Obstacle::Nothing, vector<Wall>{Wall::WallNotExists, Wall::WallExists, Wall::WallNotExists, Wall::WallNotExists}));
-	_tilevec.push_back(TileInfo(Point(-2, 2), Obstacle::Nothing, vector<Wall>{Wall::WallExists, Wall::WallExists, Wall::WallNotExists, Wall::WallNotExists}));*/
-}
-
-vector<TileInfo> TileManager::GetTiles()
-{
-	return _tilevec;
-}
